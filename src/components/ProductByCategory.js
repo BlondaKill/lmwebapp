61c365c3755a5581aac296a268ceb4c94b5b6184
lauramorales <ma.lauramorales@gmail.com>
2024-02-09 18:45:51 -0300

    img

--- src/components/ProductByCategory.js
+++ src/components/ProductByCategory.js
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import colors from '../utility/globals/colors'
 
 
 const ProductByCategory = ({item}) => {
-  return (
-    <View style={styles.container}>
-      <Text>{item.id} {item.title}</Text>
-    </View>
-  )
+    return (
+        <View style={styles.container}>
+            <Image style={styles.image} source={{uri:item.thumbnail}}/>
+            <Text style={styles.text}>{item.id} {item.title}</Text>
+        </View>
+    )
 }
+
+//require("../../assets/crep.jpeg")
 
 export default ProductByCategory
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: colors.primary,
+        width:"80%",
+        marginHorizontal: "10%",
+        marginVertical: 10,
+        padding: 10,
+        borderRadius: 5,
+        flexDirection: "row",
+        justifyContent:"center",
+        alignItems:"center",
+        gap: 20
+    },
+    text:{
+        width: "60%",
+        fontSize:16,
+    },
+    image:{
+        width: 90,
+        height: 90,
+    }
+
+
+
+
+})
