fa4093bdef82e5fab0958004134e6341013e826f
lauramorales <ma.lauramorales@gmail.com>
2024-02-09 17:43:18 -0300

    carga de productos

--- src/screens/ProductsByCategory.js
+++ src/screens/ProductsByCategory.js
@@ -1,16 +1,26 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native'
 import Header from '../components/Header'
 import products from '../utility/data/products.json'
+import { useEffect, useState } from 'react'
 
 
 
 const ProductsByCategory = ({categorySelected}) => {
+
+  const [productsFiltered, setProductsFiltered] = useState([])
+
+  useEffect(() => {
+    setProductsFiltered(products.filter(product => product.category === categorySelected))
+  }, [categorySelected])
+
+
+
+
   return (
     <>
       <Header title={categorySelected}/>
       <FlatList
-
-        data={products}
+        data={productsFiltered}
         keyExtractor={item => item.id}
         renderItem={({item})=> <View><Text>{item.title}</Text></View> } 
     />
