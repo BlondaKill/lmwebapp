7f700497a47276a5cfe9ba07051dd9fffad66b8e
lauramorales <ma.lauramorales@gmail.com>
2024-02-21 14:57:25 -0300

    "CartStack"

---
+++ src/navigation/CartStack.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class CartStack extends Component {
+  render() {
+    return (
+      <View>
+        <Text>CartStack</Text>
+      </View>
+    )
+  }
+}
+
+export default CartStack
