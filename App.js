4081236cd5abeb9f628a3ff4876eaca436238a77
lauramorales <ma.lauramorales@gmail.com>
2024-02-21 12:37:01 -0300

    MainNavigator

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
-import { StatusBar, StyleSheet } from 'react-native'
-import Home from './src/screens/Home'
+import { StatusBar } from 'react-native'
 import {useFonts} from "expo-font"
 import { fontGroup } from './src/utility/globals/fonts'
 import colors from './src/utility/globals/colors'
-import ProductsByCategory from './src/screens/ProductsByCategory'
-import ProductDetail from './src/screens/ProductDetail'
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Header from './src/components/Header'
+import MainNavigator from './src/navigation/MainNavigator'
 
 
-const Stack = createNativeStackNavigator();
+
+
+
 
 const App = () => {
 
@@ -22,35 +19,11 @@ const App = () => {
   return (
     <>
       <StatusBar backgroundColor={colors.grey} barStyle={'dark-content'}/>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName='Home'
-          screenOptions={({route, navigation})=> {
-            return{
-              header: () =>{
-                return <Header 
-                              navigation={navigation}
-                              title= {route.name === "Home" ? "Rochino" : 
-                                        route.name === "ProductsByCategory" ? route.params.categorySelected : "Detalle"
-              }/>
-              }
-            }
-          }
-        }
-        >
-          <Stack.Screen name="Home" component={Home}/>
-          <Stack.Screen name="ProductsByCategory" component={ProductsByCategory}/>
-          <Stack.Screen name="ProductDetail" component={ProductDetail}/>         
-        </Stack.Navigator> 
-      </NavigationContainer>
+      <MainNavigator/>
     </>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-  }
-})
+
---
+++ src/navigation/MainNavigator.js
@@ -0,0 +1,37 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Header from '../components/Header'
+import ProductsByCategory from '../screens/ProductsByCategory'
+import ProductDetail from '../screens/ProductDetail'
+import Home from '../screens/Home'
+
+
+const Stack = createNativeStackNavigator();
+
+const MainNavigator = () => {
+    return (
+        <NavigationContainer>
+                <Stack.Navigator
+                    initialRouteName='Home'
+                    screenOptions={({route, navigation})=> {
+                        return{
+                        header: () =>{
+                            return <Header 
+                                        navigation={navigation}
+                                        title= {route.name === "Home" ? "Rochino" : 
+                                                route.name === "ProductsByCategory" ? route.params.categorySelected : "Detalle"
+                    }/>
+                    }
+                    }
+                }}
+            >
+            <Stack.Screen name="Home" component={Home}/>
+            <Stack.Screen name="ProductsByCategory" component={ProductsByCategory}/>
+            <Stack.Screen name="ProductDetail" component={ProductDetail}/>         
+            </Stack.Navigator> 
+        </NavigationContainer>
+        
+    )
+}
+
+export default MainNavigator
