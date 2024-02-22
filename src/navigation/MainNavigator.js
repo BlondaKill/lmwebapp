cd8eaebee33d2ed69500b25945ae18d7986558a0
lauramorales <ma.lauramorales@gmail.com>
2024-02-22 14:15:20 -0300

    styles tabBar 2 iOS

--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -5,6 +5,7 @@ import ShopStack from './ShopStack';
 import CartStack from './CartStack';
 import OrdersStack from './OrdersStack';
 import colors from '../utility/globals/colors';
+import Orders from '../screens/Orders';
 
 const Tab = createBottomTabNavigator();
 
@@ -16,11 +17,12 @@ const MainNavigator = () => {
                 screenOptions={{
                     headerShown: false,
                     tabBarShowLabel: false,
-                    tabBarStyle: styles.tabBar
+                    tabBarStyle: styles.tabBar,
                 }}
             >
                 <Tab.Screen name='ShopStack' component={ShopStack}/>
                 <Tab.Screen name='CartStack' component={CartStack}/>
+                <Tab.Screen name='OrdersStack' component={OrdersStack}/>
             </Tab.Navigator>
         </NavigationContainer>
         
@@ -32,9 +34,20 @@ export default MainNavigator
 const styles = StyleSheet.create({
     tabBar:{
         backgroundColor: colors.grey,
-        height: 60
+        height: 70,
+        elevation: 4,
+        position: "absolute",
+        bottom: 20,
+        left: 15,
+        right: 15,
+        borderRadius: 10,
+        /*iOS*/
+        shadowColor: "black",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
     }
-
-    
-
 })
