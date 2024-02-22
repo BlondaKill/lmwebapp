adecabc23ec9c56e69643dd89e892c785b72c9a6
lauramorales <ma.lauramorales@gmail.com>
2024-02-22 13:39:30 -0300

    styles tabBar

--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -1,17 +1,40 @@
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import ShopStack from './ShopStack';
 import CartStack from './CartStack';
 import OrdersStack from './OrdersStack';
+import colors from '../utility/globals/colors';
 
-
+const Tab = createBottomTabNavigator();
 
 const MainNavigator = () => {
     return (
         <NavigationContainer>
-            <OrdersStack/>
+            <Tab.Navigator
+                initialRouteName='ShopStack'
+                screenOptions={{
+                    headerShown: false,
+                    tabBarShowLabel: false,
+                    tabBarStyle: styles.tabBar
+                }}
+            >
+                <Tab.Screen name='ShopStack' component={ShopStack}/>
+                <Tab.Screen name='CartStack' component={CartStack}/>
+            </Tab.Navigator>
         </NavigationContainer>
         
     )
 }
 
 export default MainNavigator
+
+const styles = StyleSheet.create({
+    tabBar:{
+        backgroundColor: colors.grey,
+        height: 60
+    }
+
+    
+
+})
