c34a34549c5edb4f8faf7f24a5a002f6e82b15f7
lauramorales <ma.lauramorales@gmail.com>
2024-02-23 11:45:55 -0300

    cart.json

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -1,14 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import cart from '../utility/data/cart.json'
+import fonts from '../utility/globals/fonts'
 
 const Cart = () => {
   return (
-    <View>
-      <Text>Cart</Text>
+    <View style={styles.container}>
+        <FlatList
+        data={cart.items}
+        keyExtractor={(item)=>item.id}
+        renderItem={({item})=> <CartItem item={item}/>}
+        />
+        <View style={styles.confirmContainer}>
+            <Pressable>
+                <Text style={styles.confirmText}>Confirmar</Text>
+            </Pressable>
+            <Text style={styles.confirmText}>Total: $ {cart.total}</Text>
+        </View>
     </View>
   )
 }
 
 export default Cart
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"space-between",
+        marginBottom:130
+    },
+    confirmContainer:{
+        flexDirection:"row",
+        backgroundColor:"gray",
+        padding:25,
+        justifyContent:"space-between",
+    },
+    confirmText:{
+        fontFamily:fonts.OleoScriptRegular,
+        fontSize:18,
+        color:"white"
+    }
+})
+  
+
+
