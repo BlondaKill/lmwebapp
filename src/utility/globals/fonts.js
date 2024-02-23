651751166ee43a7980ce546019071d2f46b4d5b5
lauramorales <ma.lauramorales@gmail.com>
2024-02-23 17:33:31 -0300

    Roboto & Sen fonts

--- src/utility/globals/fonts.js
+++ src/utility/globals/fonts.js
@@ -1,14 +1,33 @@
 export const fontGroup = {
     "Cookie-Regular": require("../../../assets/fonts/Cookie-Regular.ttf"),
     "OleoScript-Bold": require("../../../assets/fonts/OleoScript-Bold.ttf"),
-    "OleoScript-Regular": require("../../../assets/fonts/OleoScript-Regular.ttf")
-
+    "OleoScript-Regular": require("../../../assets/fonts/OleoScript-Regular.ttf"),
+    "RobotoFlex-Regular": require("../../../assets/fonts/RobotoFlex-Regular"),
+    "Roboto-Black": require("../../../assets/fonts/Roboto-Black"),
+    "Roboto-Bold": require("../../../assets/fonts/Roboto-Bold"),
+    "Roboto-Light": require("../../../assets/fonts/Roboto-Light"),
+    "Roboto-Medium": require("../../../assets/fonts/Roboto-Medium"),
+    "Roboto-Regular": require("../../../assets/fonts/Roboto-Regular"),
+    "Sen-VariableFont_wght": require("../../../assets/fonts/Sen-VariableFont_wght"),
+    "Sen-Medium": require("../../../assets/fonts/Sen-Medium"),
+    "Sen-Regular": require("../../../assets/fonts/Sen-Regular"),
+    "Sen-SemiBold": require("../../../assets/fonts/Sen-SemiBold"),
 }
 
 const fonts = {
     CookieRegular : "Cookie-Regular",
     OleoScriptBold : "OleoScript-Bold",
-    OleoScriptRegular : "OleoScript-Regular"
+    OleoScriptRegular : "OleoScript-Regular",
+    RobotoBlack: "RobotoBlack",
+    RobotoBold: "RobotoBold",
+    RobotoLight: "RobotoLight",
+    RobotoMedium: "RobotoMedium",
+    RobotoRegular: "RobotoRegular",
+    RobotoFlexRegular: "RobotoFlexRegular",
+    SenVariableFontWght: "SenVariableFontWght",
+    SenMedium : "SenMedium",
+    SenRegular : "SenRegular",
+    SenSemiBold : "SenSemiBold",
 }
 
 
