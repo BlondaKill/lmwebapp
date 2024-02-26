bd71371f1fd7e308c3abfa4d85620e613c3d8115
lauramorales <ma.lauramorales@gmail.com>
2024-02-26 12:01:07 -0300

    counterSlice

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,18 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 import React, { useState } from 'react'
 
 const Counter = () => {
 
+    const [number, setNumber] = useState(0)
     const [count, setCount] = useState(0)
+
     return (
         <View style={styles.counterContainer}>
             <Button title='+' onPress={()=> setCount(count + 1)}/>
             <Text>{count}</Text>
             <Button title='-'  onPress={ ()=> setCount(count - 1) }/>
+            <TextInput style={styles.input} onChangeText={ (t) => setNumber(parseInt(t)) }/>
+            <Button title='monto' onPress={ ()=> setCount(number) } />
         </View>
     )
 }
