7c2962eb3ce06c40d0c835cce06d406cab1b97ba
lauramorales <ma.lauramorales@gmail.com>
2024-03-02 14:02:10 -0300

    AuthStack-Register-Login

---
+++ src/navigation/AuthStack.js
@@ -0,0 +1,28 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Login from '../screens/Login'
+
+
+const Stack = createNativeStackNavigator()
+
+
+const AuthStack = () => {
+    return (
+        <Stack.Navigator>
+            initialRouteName= 'Login'
+            screenOptions={({navigation, route})=>{
+                return {
+                    header: () => <Header 
+                        title= {route.name === "Login" ? "Inicio de sesion" : "Registro"}
+                        navigation={navigation}
+                        />
+            }
+        }}
+        <Stack.Screen name = 'Login' component={Login}/>
+        <Stack.Screen name = 'Register' component={Register}/>
+
+
+    </Stack.Navigator>
+    )
+}
+
+export default AuthStack
