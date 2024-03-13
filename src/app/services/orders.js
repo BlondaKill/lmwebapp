6e5a29e93e6798c59955d6e02183c59d9c749383
lauramorales <ma.lauramorales@gmail.com>
2024-03-13 17:38:48 -0300

    error correccion orders.js baseUrl

--- src/app/services/orders.js
+++ src/app/services/orders.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const ordersApi = createApi({
     reducerPath:"ordersApi",
-    baseQuery: fetchBaseQuery({baseUrl:"https://dappweb-dbd08-default-rtdb.firebaseio.com/"}),
+    baseQuery: fetchBaseQuery({baseUrl:"https://dappweb-dbd08-default-rtdb.firebaseio.com"}),
     tagTypes:["Orders"],
     endpoints:(builder)=>({
         postOrder:builder.mutation({
